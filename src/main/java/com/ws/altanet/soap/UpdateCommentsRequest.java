87582cce0f8aee3e318fe111c56df0df4243fb60
ksggroup <ksggroup000@gmail.com>
2017-03-11 00:30:10 +0800

    commentUpdatedRequest

--- src/main/java/com/ws/altanet/soap/UpdateCommentsRequest.java
+++ src/main/java/com/ws/altanet/soap/UpdateCommentsRequest.java
@@ -13,8 +13,7 @@ public class UpdateCommentsRequest implements  Serializable {
 	@XmlElement(name = "comment_id", required = true)
 	private Long comment_id;
 	private String content;
-	private Long post_id;
-	private Long user_id;
+	
 	
 	public Long getComment_id() {
 		return comment_id;
@@ -28,16 +27,5 @@ public class UpdateCommentsRequest implements  Serializable {
 	public void setContent(String content) {
 		this.content = content;
 	}
-	public Long getPost_id() {
-		return post_id;
-	}
-	public void setPost_id(Long post_id) {
-		this.post_id = post_id;
-	}
-	public Long getUser_id() {
-		return user_id;
-	}
-	public void setUser_id(Long user_id) {
-		this.user_id = user_id;
-	}
+	
 }
